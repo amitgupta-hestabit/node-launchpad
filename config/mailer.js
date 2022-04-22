99d8e17d83c5e38fb9e92a103163280116071724
amitgupta-hestabit <amitg.hestabit@gmail.com>
2022-04-22 19:29:10 +0530

    changes

--- config/mailer.js
+++ config/mailer.js
@@ -3,12 +3,12 @@ var nodemailer = require("nodemailer");
 
   // create reusable transporter object using the default SMTP transport
   module.exports = transport = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
     secure: false, // true for 465, false for other ports
     requireTLS:true,
     auth: {
-      user: 'amit.testg@gmail.com', // generated ethereal user
-      pass: 'Amit123!@#', // generated ethereal password
+      user: process.env.SMTP_USER, // generated ethereal user
+      pass: process.env.SMTP_PASSORD, // generated ethereal password
     },
   });
